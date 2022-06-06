f75a5a7a8260c5abaa278c93a7b1aa0d40e8d315
Hayden West <haydenw7@gmail.com>
2022-06-06 16:20:52 -0700

    fixed server bug

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 const port = 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
