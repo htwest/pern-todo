6aa28782ce0e5615446d67bf4817a951bbc71e2b
Hayden West <haydenw7@gmail.com>
2022-06-07 11:22:34 -0700

    created PUT method`

--- server/index.js
+++ server/index.js
@@ -58,4 +58,19 @@ app.get("/todos/:id", async (req, res) => {
 
 // Update a Todo
 
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was Updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 // Delete a Todo
