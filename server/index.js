dc61812cd0dfd02b8465c4e4fd9e1e172b9aeeb9
Hayden West <haydenw7@gmail.com>
2022-06-07 10:58:46 -0700

    created POST todo Route

--- server/index.js
+++ server/index.js
@@ -6,10 +6,35 @@ const pool = require("./db");
 
 const port = 5000;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
+
+// ROUTES
+
+// Create Todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1)",
+      [description]
+    );
+
+    res.json(newTodo);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+// Get All Todo's
+
+// Get a Todo
+
+// Update a Todo
+
+// Delete a Todo
