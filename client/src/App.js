8dd5362626d75d0d7da0c3f630a629e5bc75322d
Hayden West <haydenw7@gmail.com>
2022-06-07 12:21:53 -0700

    created INPUT TODO

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import { Fragment } from "react";
 import "./App.css";
 
+// Components
+import InputTodo from "./components/InputTodo";
+
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <InputTodo />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const InputTodo = () => <h1>Input Todo</h1>;
+
+export default InputTodo;
