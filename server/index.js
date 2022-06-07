edc7075a4a8336b7df88ef3202ac9b11695f045c
Hayden West <haydenw7@gmail.com>
2022-06-07 11:14:11 -0700

    created GET todo from ID

--- server/index.js
+++ server/index.js
@@ -44,6 +44,18 @@ app.get("/todos", async (req, res) => {
 
 // Get a Todo
 
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 // Update a Todo
 
 // Delete a Todo
